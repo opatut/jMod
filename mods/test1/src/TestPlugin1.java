cd696a27e3ea828b5bdf33de08729e6cb4c77856
v0rtex <karamel1@hotmail.de>
2011-02-13 13:01:34 +0100

    Added GuiScreen test.

--- mods/test1/src/TestPlugin1.java
+++ mods/test1/src/TestPlugin1.java
@@ -1,7 +1,17 @@
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import JMod.EventType;
+import JMod.JoinServerGuiScreen;
 import JMod.ModListener;
 import JMod.Plugin;
+import net.minecraft.src.GuiInventory;
+import net.minecraft.src.SoundManager;
+
 import org.lwjgl.input.Keyboard;
+
+import paulscode.sound.*;
 
 
 public class TestPlugin1 extends Plugin {
@@ -17,6 +27,9 @@ public class TestPlugin1 extends Plugin {
 			ModListener.getInstance().AddHook(EventType.BlockPlaced, this, 
 					getClass().getDeclaredMethod("OnBlockPlaced", int.class,int.class,int.class,int.class));
 			
+			ModListener.getInstance().AddHook(EventType.KeyReleased, this, 
+					getClass().getDeclaredMethod("OnKeyReleased", int.class));
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -27,14 +40,50 @@ public class TestPlugin1 extends Plugin {
 	}
 	
 	public void OnBlockPlaced(int itemID,int posX,int posY,int posZ) {
-		System.out.println("You placed a " + itemID + " on the position ("+posX+","+posY+","+posZ+")");
+		String ItemName = "";
+		
+		switch(itemID){
+		case 1:
+			ItemName = "stone";
+			break;
+		case 2:
+			ItemName = "grass";
+			break;
+		case 3:
+			ItemName = "dirt";
+			break;
+		case 4:
+			ItemName = "cobblestone";
+			break;
+		case 5:
+			ItemName = "wooden plank";
+			break;
+		case 12:
+			ItemName = "sand";
+			break;
+		case 13:
+			ItemName = "gravel";
+			break;
+		}
+		
+		
+		System.out.println("You placed a " + ItemName + " block on the position ("+posX+","+posY+","+posZ+")");
 	}
 	
 	public void OnKeyPressed(int key) {
-		
 		if(key == Keyboard.KEY_E){
-			System.out.println("You pressed E on your keyboard.");
+			
+			ModListener.getInstance().mc.displayGuiScreen(new JoinServerGuiScreen());
+			
+			//SoundSystem sound = SoundManager.sndSystem;
+			//try {
+				//sound.backgroundMusic("test", new URL("http://localhost/exp_loop_2.wav"), "exp_loop_2.wav", true);
+			//} catch (IOException e) {}
 		}
+	}
+	
+	public void OnKeyReleased(int key) {
+		System.out.println("Released: "+key);
 	}
 		
 	public String GetName() {
