b169174731776c6c382b22e1abda935b28e8ef40
opatut <opatutlol@aol.com>
2011-02-13 12:34:18 +0100

    New try.

--- src/makepatch.sh
+++ src/makepatch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #diff -r -u minecraft_original minecraft > mc.patch
-diff -ur ./minecraft_original ./minecraft > mc.patch
+#diff -ur ./minecraft_original ./minecraft > mc.patch
+wineconsole makepatch.bat
